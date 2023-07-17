54b314de7eb942bea93211de2cb60566e93c7884
Tien <ngominhtien221102@gmail.com>
2023-07-17 20:49:58 +0700

    Commit

---
+++ SWD392_PracinicalManagement/Pages/PracinicalCategory/Manager.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWD392_PracinicalManagement.Pages.PracinicalCategory
+{
+    public class ManagerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
